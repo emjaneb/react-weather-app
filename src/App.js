da8d7c00e9b1f4824f13cd87f56f132362fc2de2
emjaneb <137766632+emjaneb@users.noreply.github.com>
2024-06-21 16:37:31 +1000

    added footer link to github repository

--- src/App.js
+++ src/App.js
@@ -8,6 +8,16 @@ function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Search />
+      <footer>
+        <a
+          href="https://github.com/emjaneb/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by <b>Emma Bartley</b>
+      </footer>
     </div>
   );
 }
