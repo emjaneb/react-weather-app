1a2fb2bae28eb8eabf78fb82d0c4cf56802e3708
emjaneb <137766632+emjaneb@users.noreply.github.com>
2024-06-25 12:00:57 +1000

    Moved bootstrap npm import from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
 import Search from "./Search";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <Search />
-      <footer>
-        <a
-          href="https://github.com/emjaneb/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by <b>Emma Bartley</b>
-      </footer>
+      <div className="container">
+        <h1>Weather Search Engine</h1>
+        <Search />
+
+        <footer>
+          <a
+            href="https://github.com/emjaneb/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by <b>Emma Bartley</b>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
